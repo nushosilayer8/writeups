882155f0dbe962d81d786de913b0c0813880440d
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2018-06-18 14:01:47 +0800

    Deployed 9559034 with MkDocs version: 0.17.2

--- crossctf-finals2018/re/perfect/perfect.py
+++ crossctf-finals2018/re/perfect/perfect.py
@@ -1,4 +1,5 @@
 import random
+from pwn import *
 
 nums=[107, 127, 521, 521, 607, 1279]
 
@@ -6,14 +7,8 @@ def perfectexp(p): return (2**(p-1))*(2**p-1)
 
 def run(x):
     random.seed(perfectexp(x))
-
-    bys = [random.randint(0, 255) for i in range(35)]
-
-    k= ''.join(map(chr, bys))
-
-    print(hex(int(k.encode('hex'), 16) ^ 0x754e26ccd4b1bfafb3ffbdaa748780b7f0e0c3ae9acc3c008670f0fafd34f8ffa596db))
-
+    k = "".join([chr(random.randint(0, 255)) for i in range(35)])
+    print(xor(k, '754e26ccd4b1bfafb3ffbdaa748780b7f0e0c3ae9acc3c008670f0fafd34f8ffa596db'.decode('hex')))
 
 for i in nums:
     run(i)
-
